6474a2616045d9760cb85484a1446afa74a5b16d
motu55 <david.simmen@bluewin.ch>
2019-01-28 20:51:34 +0100

    Update urls.py

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from {{cookiecutter.project_slug}} import views
 
 urlpatterns = [
-    url(r'^{{cookiecutter.initials}}/home$', views.home)
+    url(r'^{{cookiecutter.project_initials}}/home$', views.home)
 ]

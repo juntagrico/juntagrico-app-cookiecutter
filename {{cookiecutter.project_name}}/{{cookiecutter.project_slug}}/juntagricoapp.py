a6e8e5eb75da1aae83f21b8213db166e2ee16a68
David Glenck <github@ihdg.ch>
2021-04-05 17:22:12 +0200

    fix typo

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/juntagricoapp.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/juntagricoapp.py
@@ -3,4 +3,4 @@ import {{cookiecutter.project_slug}}
 
 
 addons.config.register_user_menu('{{cookiecutter.project_initials}}/menu.html')
-addons.config.registerversion({{cookiecutter.project_slug}}.name, {{cookiecutter.project_slug}}.version)
+addons.config.register_version({{cookiecutter.project_slug}}.name, {{cookiecutter.project_slug}}.version)

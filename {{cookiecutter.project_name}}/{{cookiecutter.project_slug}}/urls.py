cb251ba627e52afb4e65ba657f4a06f58f813da0
David Glenck <pascal.free@ihdg.ch>
2022-02-10 23:35:05 +0100

    use url namespaces and names

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from {{cookiecutter.project_slug}} import views
 
+app_name = '{{cookiecutter.project_initials}}'
 urlpatterns = [
-    path('{{cookiecutter.project_initials}}/home', views.home)
+    path('{{cookiecutter.project_initials}}/home', views.home, name='home')
 ]
